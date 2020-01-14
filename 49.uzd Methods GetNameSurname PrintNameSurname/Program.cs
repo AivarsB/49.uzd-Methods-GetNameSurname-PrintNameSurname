f1f7b6803ea45c28ec163abb219b0c0bb1a843be
AivarsB <aivars.berzins@inbox.lv>
2020-01-14 17:29:50 +0200

    Add project files.

---
+++ 49.uzd Methods GetNameSurname PrintNameSurname/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _48._uzd_Method_PrintMyName
+{
+    class Program
+    {
+        static void Main()
+        {
+            PrintNameSurname(GetNameSurname());
+        }
+
+        static string GetNameSurname()
+        {
+            Console.Write("Enter name: ");
+            var name = Console.ReadLine();
+
+            Console.Write("Enter surname: ");
+            var surname = Console.ReadLine();
+
+            return $"{name} {surname}";
+
+        }
+        static void PrintNameSurname(string FullName)
+        {
+            Console.WriteLine(FullName);
+        }
+    }
+}
+// Izveidot divas metodes GetNameSurname un PrintNameSurname. 
+// Metodei GetNameSurname ir no lietotāja jāpaprasa vārds uzvārds un kā string jāatgriež metodei PrintNameSurname, 
+// kura šo tekstu izdrukās console.
